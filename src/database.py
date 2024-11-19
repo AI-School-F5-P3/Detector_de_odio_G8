85e76a5b7c5b49be66d9ac950eda5206b917bd75
Mª Rosa Cuenca <mariarosacuenca@gmail.com>
2024-11-19 14:34:03 +0100

    ajuste base de datos

--- src/database.py
+++ src/database.py
@@ -42,8 +42,8 @@ class DatabaseManager:
             id SERIAL PRIMARY KEY,
             video_id VARCHAR(50) NOT NULL,
             comment_id VARCHAR(100) NOT NULL,
-            traditional_hate SMALLINT NOT NULL,
-            transformer_hate SMALLINT NOT NULL,
+            traditional_hate SMALLINT,
+            transformer_hate SMALLINT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             UNIQUE(video_id, comment_id)
         );
